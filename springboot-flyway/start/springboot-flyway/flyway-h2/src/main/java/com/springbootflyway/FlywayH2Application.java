7226b16b886fac4fec31ae0b09c0dc7b03859076
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2022-03-17 10:07:29 -0500

    added the space

--- springboot-flyway/start/springboot-flyway/flyway-h2/src/main/java/com/springbootflyway/FlywayH2Application.java
+++ springboot-flyway/start/springboot-flyway/flyway-h2/src/main/java/com/springbootflyway/FlywayH2Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlywayH2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlywayH2Application.class, args);
+        SpringApplication.run(FlywayH2Application.class,
+                args);
     }
 
 }
