461a6dc5bb8eb7255ac1c860e2cd364c87da07fe
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2022-03-17 12:47:37 -0500

    added the postgres project for flyway

---
+++ springboot-flyway/final/springboot-flyway/flyway-postgres/src/main/java/com/springbootflyway/FlywayPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.springbootflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayPostgresApplication.class, args);
+    }
+
+}
